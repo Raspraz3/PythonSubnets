55f9103a17bdf6c1403e9b1f3c7b6dc8be6e738c
Raspraz3 <147226223+Raspraz3@users.noreply.github.com>
2024-03-30 23:43:58 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 46640
+
+#problem 1
+p1Network = '80.38.89.0'
+p1Broadcast = '80.38.89.255'
+p1First = '80.38.89.1'
+p1Last =  '80.38.89.254'
+
+#problem 2
+p2Network = '247.24.51.144'
+p2Broadcast = '247.24.51.159'
+p2First = '247.24.51.145'
+p2Last = '247.24.51.158'
+
+#problem 3
+p3Network = '158.9.12.128'
+p3Broadcast = '158.9.12.255'
+p3First = '158.9.12.129'
+p3Last = '158.9.12.255'
+
+#problem 4
+p4Network = '69.250.230.232'
+p4Broadcast = '69.250.230.239'
+p4First = '69.250.230.233'
+p4Last = '69.250.230.238'
+
+#problem 5
+p5Network = '236.235.192.0'
+p5Broadcast = '236.235.192.63'
+p5First = '236.235.192.1'
+p5Last = '236.235.192.62'
+
+#problem 6
+p6Network = '147.220.154.0'
+p6Broadcast = '147.220.154.255'
+p6First = '147.220.154.1'
+p6Last =  '147.220.154.254'
+
+#problem 7
+p7Network = '58.205.115.96'
+p7Broadcast = '58.205.115.127'
+p7First = '58.205.115.97'
+p7Last = '58.205.115.126'
+
+#problem 8
+p8Network = '225.190.77.128'
+p8Broadcast = '225.190.77.255'
+p8First = '225.190.77.129'
+p8Last = '225.190.77.254'
+
+#problem 9
+p9Network = '136.175.39.192'
+p9Broadcast = '136.175.39.207'
+p9First = '136.175.39.193'
+p9Last = '136.175.39.206'
+
+#problem 10
+p10Network = '47.161.1.0'
+p10Broadcast = '47.161.1.255'
+p10First = '47.161.1.1'
+p10Last = '47.161.1.254'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
